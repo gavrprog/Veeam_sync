1b2715153a60b9cf5f75613a667a683340d622e5
51444218+gavrprog@users.noreply.github.com <gavr.prog@gmail.com>
2022-10-24 09:14:09 +0200

    added comment to the code block

--- sync.py
+++ sync.py
@@ -26,21 +26,23 @@ while 1:
     src_arr, dst_arr, del_arr = [], [], []
     del_root(root_src_dir, list_dirs(root_src_dir, src_arr))
     del_root(root_dst_dir, list_dirs(root_dst_dir, dst_arr))
-    if dst_arr:  # если клон не новый
+
+    if dst_arr:  # if source is not new
         for path_src in src_arr:
             count = len(dst_arr)
             full_path_src = root_src_dir + path_src
             for path_dst in dst_arr:
                 full_path_dst = root_dst_dir + path_dst
-                # перебираем пути и, если совпало и это новый файл - перезаписываем, если это папка - пропускаем
-                if path_src == path_dst:
+
+                if path_src == path_dst:  # sort pathes and if the new file is found - rewrite
                     if os.path.isfile(full_path_src):
                         if os.path.getmtime(full_path_src) > os.path.getmtime(full_path_dst):
                             shutil.copy2(full_path_src, root_dst_dir + os.path.split(path_dst)[0])
                             logger.add_log(full_path_dst, 2)
                 else:
                     count -= 1
-            if count == 0:  # если count=0  - новый путь. Создаем копию файла или папки
+
+            if count == 0:  # if count=0  - new path. Create a folder or a file
                 if os.path.isfile(full_path_src):
                     shutil.copy2(full_path_src, root_dst_dir + os.path.split(path_src)[0])
                     logger.add_log(root_dst_dir + path_src, 1)
@@ -48,7 +50,7 @@ while 1:
                     os.mkdir(root_dst_dir + path_src)
                     logger.add_log(root_dst_dir + path_src, 1)
 
-        for path_dst in dst_arr:  # определяем массив с папками, которые надо удалить из источника
+        for path_dst in dst_arr:  # determine array with pathes which is needed to delete from source
             flag = False
             for path_src in src_arr:
                 if path_dst == path_src:
@@ -56,7 +58,7 @@ while 1:
             if not flag:
                 del_arr.append(path_dst)
 
-        for i in range(len(del_arr), 0, -1):  # удаляем из источника папки и файлы
+        for i in range(len(del_arr), 0, -1):  # delete folders and files from source
             path = root_dst_dir + del_arr[i - 1]
             if os.path.isfile(path):
                 os.remove(path)
@@ -65,7 +67,7 @@ while 1:
                 os.rmdir(path)
                 logger.add_log(path, 0)
 
-    else:  # если клон новый
+    else:  # if source is new
         for path in src_arr:
             if os.path.isdir(root_src_dir + path):
                 os.mkdir(root_dst_dir + path)
